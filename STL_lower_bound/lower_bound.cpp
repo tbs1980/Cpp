e97b90929b1526bf6e1c6bf22b6e437db89e03ff
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2014-05-29 19:15:46 +0100

    STL lower_bound example added.

---
+++ STL_lower_bound/lower_bound.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main(void)
+{
+
+	std::vector<int> v(10);
+
+	for(size_t i=0;i<v.size();++i)
+	{
+		v[i] =  i;
+	}
+
+
+	std::vector<int>::iterator low;
+
+	int val=-1;
+	low = std::lower_bound (v.begin(), v.end(), val);
+	std::cout<<"val= "<<val<<" position of lower_bound = "<<low-v.begin()<<std::endl;
+
+	val=0;
+	low = std::lower_bound (v.begin(), v.end(), val);
+	std::cout<<"val= "<<val<<" position of lower_bound = "<<low-v.begin()<<std::endl;
+
+	val=5;
+	low = std::lower_bound (v.begin(), v.end(), val);
+	std::cout<<"val= "<<val<<" position of lower_bound = "<<low-v.begin()<<std::endl;
+
+	val=9;
+	low = std::lower_bound (v.begin(), v.end(), val);
+	std::cout<<"val= "<<val<<" position of lower_bound = "<<low-v.begin()<<std::endl;
+
+	val=12;
+	low = std::lower_bound (v.begin(), v.end(), val);
+	std::cout<<"val= "<<val<<" position of lower_bound = "<<low-v.begin()<<std::endl;
+
+	return 0;
+}
+
