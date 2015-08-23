4a7baa7fad384450dbed631e801ed803ca6a4cba
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2015-08-23 20:43:13 +0100

    numerical diff example now working

--- EigenNumericalDiff/numDiff.cpp
+++ EigenNumericalDiff/numDiff.cpp
@@ -2,74 +2,89 @@
 #include <iostream>
 #include <unsupported/Eigen/NumericalDiff>
 
-template<typename _Scalar>
+template<typename _Scalar, int NX=Eigen::Dynamic, int NY=Eigen::Dynamic>
 class NormDistFunctor
 {
 public:
     typedef _Scalar Scalar;
-    typedef Eigen::Matrix<Scalar,Eigen::Dynamic,1> InputType;
-    typedef Eigen::Matrix<Scalar,Eigen::Dynamic,1> ValueType;
-    typedef Eigen::Matrix<Scalar,Eigen::Dynamic,1> JacobianType;
+    enum
+    {
+        InputsAtCompileTime = NX,
+        ValuesAtCompileTime = NY
+    };
+
+    typedef Eigen::Matrix<Scalar,InputsAtCompileTime,1> InputType;
+    typedef Eigen::Matrix<Scalar,ValuesAtCompileTime,1> ValueType;
+    typedef Eigen::Matrix<Scalar,ValuesAtCompileTime,InputsAtCompileTime> JacobianType;
     typedef typename InputType::Index Index;
+
+    int m_inputs, m_values;
+
+    NormDistFunctor()
+    : m_inputs(InputsAtCompileTime), m_values(ValuesAtCompileTime)
+    {
+
+    }
+
+    NormDistFunctor(int inputs, int values)
+    : m_inputs(inputs), m_values(values)
+    {
+
+    }
+
+    int inputs() const
+    {
+        return m_inputs;
+    }
+
+    int values() const
+    {
+        return m_values;
+    }
 
     void operator()(InputType const & x, ValueType & fx) const
     {
-        fx(0) = 0;
+        fx(0,0) = 0;
         for(Index i=0;i<x.rows();++i)
         {
-            fx(0) -= x(i)*x(i);
+            fx(0,0) -= x(i)*x(i);
         }
 
-        fx(0) *= 0.5;
-    }
-
-    Index values() const
-    {
-        return Index(1);
-    }
-
-    static const Index InputsAtCompileTime = 0;
-    static const Index ValuesAtCompileTime = 1;
-
-    Index numDims() const
-    {
-        return 10;
+        fx(0,0) *= 0.5;
     }
 
     void derivs(InputType const & x, JacobianType & dx)
     {
-        return x;
+        dx = -x.transpose();
     }
 
-    /*
-    ValueType values() const
-    {
-        ValueType fx(1);
-        fx(0) = 0;
-        return fx;
-    }*/
 };
 
 int main(void)
 {
-    typedef NormDistFunctor<double> sineFunctorType;
+    typedef NormDistFunctor<double,Eigen::Dynamic,1> sineFunctorType;
     typedef Eigen::NumericalDiff<sineFunctorType>  numDiffType;
     typedef sineFunctorType::InputType InputType;
     typedef sineFunctorType::JacobianType JacobianType;
     typedef sineFunctorType::ValueType ValueType;
 
+
+    const int numDims = 10;
+
     sineFunctorType sf;
     numDiffType nd;
 
-    InputType x = InputType::Random(sf.numDims());
-    JacobianType dx(sf.numDims());
-
-    //ValueType val;
-    //val.resize(sf.values());
+    InputType x = InputType::Random(numDims);
+    JacobianType dx(1,numDims);
 
     nd.df(x,dx);
 
-    //std::cout<<dx<<std::endl;
+    std::cout<<dx<<std::endl;
+
+    JacobianType dxExpected(1,numDims);
+    sf.derivs(x,dxExpected);
+
+    std::cout<<dxExpected<<std::endl;
 
     return 0;
 }
