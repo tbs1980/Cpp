621751bfe452754b1bada349d1e1b8c53795181b
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2015-08-27 15:01:18 +0100

    modified abs as template args

--- CmathAbs/mathabs.cpp
+++ CmathAbs/mathabs.cpp
@@ -1,8 +1,21 @@
 #include <cmath>
 #include <iostream>
 
+// g++ -std=c++98 -pedantic -Wall mathabs.cpp
+
+template<typename realScalarType>
+realScalarType testAbs(realScalarType const & x)
+{
+    using std::abs;
+
+    return abs(x);
+}
+
 int main(void)
 {
-	double x=-2.;
-	std::cout<<x<<"\t"<<std::abs(x)<<std::endl;
+    float x(2.9);
+    double y(-4.5);
+    long double z(-1e9);
+
+    std::cout<<testAbs(x)<<"\t"<<testAbs(y)<<"\t"<<testAbs(z)<<std::endl;
 }
